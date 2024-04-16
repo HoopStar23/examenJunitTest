03dca85e850308bbadcd156ffebdddb610bd9c6e
dam_sh <sb.hakeem99@gmail.com>
2024-04-16 13:43:19 +0200

    Ajuste clase Money

--- src/es/iesmz/tests/Money.java
+++ src/es/iesmz/tests/Money.java
@@ -3,12 +3,19 @@ package es.iesmz.tests;
 public class Money {
     private TipoMoneda moneda;
     private float cantidad;
-    private final float eurConvusd = 1.18798f;
-    private final float usdConveur = 0.841815f;
-    private final float eurConvgbp = 0.857839f;
-    private final float gbpConveur = 1.165826f;
+    private final float K_EUR_CONV_USD = 1.18798f;
+    private final float K_USD_CONV_EUR = 0.841815f;
+    private final float K_EUR_CONV_GBP = 0.857839f;
+    private final float K_GBP_CONV_EUR = 1.165826f;
+
+    public Money(TipoMoneda moneda, float cantidad) {
+        this.moneda = moneda;
+        this.cantidad = cantidad;
+    }
 
     public Money() {
+        this.moneda = TipoMoneda.EUR;
+        this.cantidad = 0;
     }
 
     public float change(TipoMoneda origen, TipoMoneda destino, float money){
@@ -22,17 +29,17 @@ public class Money {
 
     public float conversion(TipoMoneda origen, TipoMoneda destino, float money){
         if (origen == TipoMoneda.EUR && destino == TipoMoneda.USD) {
-            money = money*eurConvusd;
+            money = money*K_EUR_CONV_USD;
         }else if (origen == TipoMoneda.USD && destino == TipoMoneda.EUR) {
-            money = money*usdConveur;
+            money = money*K_USD_CONV_EUR;
         }else if (origen == TipoMoneda.EUR && destino == TipoMoneda.GBP) {
-            money = money*eurConvgbp;
+            money = money*K_EUR_CONV_GBP;
         }else if (origen == TipoMoneda.GBP && destino == TipoMoneda.EUR) {
-            money = money*gbpConveur;
+            money = money*K_GBP_CONV_EUR;
         }else if (origen == TipoMoneda.GBP && destino == TipoMoneda.USD) {
-            money = (money*gbpConveur) * eurConvusd;
+            money = (money*K_GBP_CONV_EUR) * K_EUR_CONV_USD;
         }else if (origen == TipoMoneda.USD && destino == TipoMoneda.GBP) {
-            money = (money*usdConveur) * eurConvgbp;
+            money = (money*K_USD_CONV_EUR) * K_EUR_CONV_GBP;
         }
         return money;
     }
